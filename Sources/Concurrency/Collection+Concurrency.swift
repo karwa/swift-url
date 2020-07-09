5c1e1e7fe7c24ded5dfe2c39f80720f6395784b1
Karl <5254025+karwa@users.noreply.github.com>
2020-07-09 20:46:03 +0200

    Swift-format concurrency, clean up test a bit

--- Sources/Concurrency/Collection+Concurrency.swift
+++ Sources/Concurrency/Collection+Concurrency.swift
@@ -3,55 +3,54 @@ import Dispatch
 // TODO:
 // - Concurrent sort (+ filter?)
 // - Collection sampling (pick n random elements without replacement)
-// - 
 
 extension RandomAccessCollection {
 
-    /// Returns a view of this collection with concurrent implementations of certain algorithms.
-    ///
-    public var concurrent: ConcurrentCollectionOps<Self> {
-        return ConcurrentCollectionOps(wrapping: self)
-    }
+  /// A namespace for concurrent operations on this collection.
+  ///
+  public var concurrent: ConcurrentCollectionOps<Self> {
+    return ConcurrentCollectionOps(self)
+  }
 }
 
 public struct ConcurrentCollectionOps<Base> where Base: RandomAccessCollection {
-    var base: Base
+  public var base: Base
 
-    init(wrapping base: Base) {
-        self.base = base
-    }
+  public init(_ base: Base) {
+    self.base = base
+  }
 }
 
 extension ConcurrentCollectionOps {
 
-    fileprivate func _forEachOffset(_ invoke: (Int)->Void) {
-        DispatchQueue.concurrentPerform(iterations: base.count) { i in invoke(i) }
-    }
+  fileprivate func _forEachOffset(_ invoke: (Int) -> Void) {
+    DispatchQueue.concurrentPerform(iterations: base.count) { i in invoke(i) }
+  }
 
-    fileprivate func _forEachIndex(_ invoke: (Base.Index)->Void) {
-        _forEachOffset { i in
-            invoke(base.index(base.startIndex, offsetBy: i))
-        }
+  fileprivate func _forEachIndex(_ invoke: (Base.Index) -> Void) {
+    _forEachOffset { i in
+      invoke(base.index(base.startIndex, offsetBy: i))
     }
+  }
 }
 
 // foreach, map.
 
 extension ConcurrentCollectionOps {
 
-    public func forEach(_ invoke: (Base.Element)->Void) {
-        _forEachIndex { invoke(base[$0]) }
-    }
+  public func forEach(_ invoke: (Base.Element) -> Void) {
+    _forEachIndex { invoke(base[$0]) }
+  }
 
-    public func map<T>(_ transform: (Base.Element)->T) -> [T] {
-        let count = base.count
-        return Array(unsafeUninitializedCapacity: count) { buffer, actualCount in
-            _forEachOffset { i in
-                let idx = base.index(base.startIndex, offsetBy: i)
-                let val = transform(base[idx])
-                (buffer.baseAddress.unsafelyUnwrapped + i).initialize(to: val)
-            }
-            actualCount = count
-        }
+  public func map<T>(_ transform: (Base.Element) -> T) -> [T] {
+    let count = base.count
+    return Array(unsafeUninitializedCapacity: count) { buffer, actualCount in
+      _forEachOffset { i in
+        let idx = base.index(base.startIndex, offsetBy: i)
+        let val = transform(base[idx])
+        (buffer.baseAddress.unsafelyUnwrapped + i).initialize(to: val)
+      }
+      actualCount = count
     }
+  }
 }
--- Tests/ConcurrencyTests/CollectionExtensionsTests.swift
+++ Tests/ConcurrencyTests/CollectionExtensionsTests.swift
@@ -1,25 +1,19 @@
-import XCTest
 import Concurrency
-
-class MyObj: CustomStringConvertible {
-    var value: Int
-    var square: Int
-    init(_ v: Int) {
-        value = v; square = v * v
-    }
-    var description: String {
-        return "\(value) -> \(square)"
-    }
-}
+import XCTest
 
 class CollectionExtensionsTestCase: XCTestCase {
-    
-    func testMap() {
-        
-        let a = 1...20
-        let b = a.concurrent.map { MyObj($0) }
-        
-//        print(b)
-        b.concurrent.forEach { print($0) }
+
+  private struct MyObject: Equatable {
+    var square: Int
+    init(_ v: Int) {
+      square = v * v
     }
+  }
+
+  func testMap() {
+    let values = 0..<50
+    let mapped = values.concurrent.map { MyObject($0) }
+    XCTAssertEqual(values.count, mapped.count)
+    XCTAssertTrue(mapped.elementsEqual(values.lazy.map { MyObject($0) }))
+  }
 }
