28e1efa75aac1230a37354e4910737865c673ba2
Karl <5254025+karwa@users.noreply.github.com>
2020-07-12 21:06:48 +0200

    Format Package.swift

--- Package.swift
+++ Package.swift
@@ -4,29 +4,29 @@
 import PackageDescription
 
 let package = Package(
-    name: "Base",
-    products: [
-        .library(name: "Base", targets: ["Base"]),
-        .library(name: "WebURL", targets: ["URL"]),
-        .executable(name: "URLBenchmarks", targets: ["URLBenchmarks"])
-    ],
-    dependencies: [
-        .package( // Swift-checkit for testing protocol conformances.
-            url: "https://github.com/karwa/swift-checkit.git",
-            .branch("master"))
-    ],
-    targets: [
-        .target(name: "Algorithms"),
-        .testTarget(name: "AlgorithmsTests", dependencies: ["Algorithms", "Checkit"]),
+  name: "Base",
+  products: [
+    .library(name: "Base", targets: ["Base"]),
+    .library(name: "WebURL", targets: ["URL"]),
+    .executable(name: "URLBenchmarks", targets: ["URLBenchmarks"]),
+  ],
+  dependencies: [
+    .package(  // Swift-checkit for testing protocol conformances.
+      url: "https://github.com/karwa/swift-checkit.git",
+      .branch("master"))
+  ],
+  targets: [
+    .target(name: "Algorithms"),
+    .testTarget(name: "AlgorithmsTests", dependencies: ["Algorithms", "Checkit"]),
 
-        .target(name: "Concurrency"),
-        .testTarget(name: "ConcurrencyTests", dependencies: ["Concurrency"]),
+    .target(name: "Concurrency"),
+    .testTarget(name: "ConcurrencyTests", dependencies: ["Concurrency"]),
 
-        .target(name: "URL", dependencies: ["Algorithms"]),
-        .target(name: "URLBenchmarks", dependencies: ["URL"]),
-        .testTarget(name: "URLTests", dependencies: ["URL"]),
-        
-        .target(name: "Base", dependencies: ["Algorithms", "Concurrency", "URL"]),
-        .testTarget(name: "BaseTests", dependencies: ["Base"]),
-    ]
+    .target(name: "URL", dependencies: ["Algorithms"]),
+    .target(name: "URLBenchmarks", dependencies: ["URL"]),
+    .testTarget(name: "URLTests", dependencies: ["URL"]),
+
+    .target(name: "Base", dependencies: ["Algorithms", "Concurrency", "URL"]),
+    .testTarget(name: "BaseTests", dependencies: ["Base"]),
+  ]
 )
