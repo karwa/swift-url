84f1624b253fb8fe980379808540da921d51bb49
Karl <5254025+karwa@users.noreply.github.com>
2021-02-04 04:28:17 +0100

    Move additional setter tests to WebURLTests. There may be an argument for moving some of them back later (if not upstreaming), as they test the JS accessor model.

--- Tests/WebURLTests/WebURLTests.swift
+++ Tests/WebURLTests/WebURLTests.swift
@@ -614,3 +614,75 @@ extension WebURLTests {
     print("ℹ️ Report written to \(reportURL)")
   }
 }
+
+extension WebURLTests {
+  
+  func testJSModelSetters() {
+    // Check that 'pathname' setter does not remove leading slashes.
+    do {
+      var x = WebURL("sc://x?hello")!.jsModel
+      XCTAssertEqual(x.href, "sc://x?hello")
+      XCTAssertEqual(x.pathname, "")
+      x.pathname = #"/"#
+      XCTAssertEqual(x.href, "sc://x/?hello")
+      XCTAssertEqual(x.pathname, "/")
+      x.pathname = #"/s"#
+      XCTAssertEqual(x.href, "sc://x/s?hello")
+      XCTAssertEqual(x.pathname, "/s")
+      x.pathname = #""#
+      XCTAssertEqual(x.href, "sc://x?hello")
+      XCTAssertEqual(x.pathname, "")
+    }
+    // Check that 'hostname' setter removes path sigil.
+    do {
+      func check_has_path_sigil(url: WebURL.JSModel) {
+        XCTAssertEqual(url.description, "web+demo:/.//not-a-host/test")
+        XCTAssertEqual(url.storage.structure.sigil, .path)
+        XCTAssertEqual(url.hostname, "")
+        XCTAssertEqual(url.pathname, "//not-a-host/test")
+      }
+      func check_has_auth_sigil(url: WebURL.JSModel, hostname: String) {
+        XCTAssertEqual(url.description, "web+demo://\(hostname)//not-a-host/test")
+        XCTAssertEqual(url.storage.structure.sigil, .authority)
+        XCTAssertEqual(url.hostname, hostname)
+        XCTAssertEqual(url.pathname, "//not-a-host/test")
+      }
+      
+      var test_url = WebURL("web+demo:/.//not-a-host/test")!.jsModel
+      check_has_path_sigil(url: test_url)
+      test_url.hostname = "host"
+      check_has_auth_sigil(url: test_url, hostname: "host")
+      test_url.hostname = ""
+      check_has_auth_sigil(url: test_url, hostname: "")
+      // We don't yet expose any way to remove an authority, so go down to URLStorage.
+      switch test_url.storage {
+      case .generic(var storage): test_url.storage = storage.setHostname(to: UnsafeBufferPointer?.none).0
+      case .small(var storage): test_url.storage = storage.setHostname(to: UnsafeBufferPointer?.none).0
+      }
+      check_has_path_sigil(url: test_url)
+    }
+    
+    // TODO [testing]: This test needs to be more comprehensive, and we need tests like this exercising all major
+    // paths in all setters.
+    
+    // Checks what happens when a scheme change requires the port to be removed, and the resulting URL
+    // has a different optimal storage type to the original (i.e. suddenly becomes less than 255 bytes).
+    // It's such a niche case that we'd otherwise never see it.
+    do {
+      var url = WebURL("ws://hostnamewhichtakesustotheedge:443?hellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellop")!.jsModel
+      switch url.storage {
+      case .generic(_): break
+      default: XCTFail("Unexpected storage type")
+      }
+      url.scheme = "wss"
+      switch url.storage {
+      case .small(_): break
+      default: XCTFail("Unexpected storage type")
+      }
+      XCTAssertEqual(
+        url.description,
+        "wss://hostnamewhichtakesustotheedge/?hellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellop"
+      )
+    }
+  }
+}
