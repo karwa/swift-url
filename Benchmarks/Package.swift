8212d68052283e6d22465ffe3020970dc6736260
Karl <5254025+karwa@users.noreply.github.com>
2021-04-09 08:29:49 +0200

    Bring in benchmarks from swift-url-tools. They should really live alongside this project.

---
+++ Benchmarks/Package.swift
@@ -0,0 +1,38 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+// Copyright The swift-url Contributors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import PackageDescription
+
+let package = Package(
+    name: "swift-url-benchmark",
+    products: [
+        .executable(name: "WebURLBenchmark", targets: ["WebURLBenchmark"])
+    ],
+    dependencies: [
+      .package(name: "Benchmark", url: "https://github.com/google/swift-benchmark", from: "0.1.0"),
+      .package(path: ".."),
+    ],
+    targets: [
+      .target(
+        name: "WebURLBenchmark",
+        dependencies: [
+          .product(name: "WebURL", package: "swift-url"),
+          .product(name: "Benchmark", package: "Benchmark")
+        ]
+      )
+    ]
+)
---
+++ Benchmarks/Sources/WebURLBenchmark/URLEncoded.swift
@@ -0,0 +1,52 @@
+// Copyright The swift-url Contributors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import Benchmark
+import WebURL
+
+/// Benchmarks the `StringProtocol.urlEncoded` and `StringProtocol.urlDecoded` properties.
+///
+let urlEncoded_Decoded = BenchmarkSuite(name: "URLEncoded") { suite in
+
+  let urlEncoded_strings = [
+    String(decoding: 0 ..< 128, as: UTF8.self), // Every ASCII character
+    #"This 🦆 is 🐧 some 🦖 m!x€d%20cºnt£nt"#,
+    #"""
+    A very long string with the occassional 🦖 emoji thrown in for good measure!
+    A very long string with the occassional 🦖 emoji thrown in for good measure!
+    A very long string with the occassional 🦖 emoji thrown in for good measure!
+    A very long string with the occassional 🦖 emoji thrown in for good measure!
+    A very long string with the occassional 🦖 emoji thrown in for good measure!
+    A very long string with the occassional 🦖 emoji thrown in for good measure!
+    A very long string with the occassional 🦖 emoji thrown in for good measure!
+    A very long string with the occassional 🦖 emoji thrown in for good measure!
+    """#
+  ]
+  suite.benchmark("String.urlEncoded") {
+    for string in urlEncoded_strings {
+      blackHole(string.urlComponentEncoded)
+    }
+  }
+
+  let urlDecoded_strings = [
+    #"%00%01%02%03%04%05%06%07%08%09%0A%0B%0C%0D%0E%0F%10%11%12%13%14%15%16%17%18%19%1A%1B%1C%1D%1E%1F%20!%22%23%24%25%26'()*%2B%2C-.%2F0123456789%3A%3B%3C%3D%3E%3F%40ABCDEFGHIJKLMNOPQRSTUVWXYZ%5B%5C%5D%5E_%60abcdefghijklmnopqrstuvwxyz%7B%7C%7D~%7F"#, // Every ASCII character
+    #"This%20%F0%9F%A6%86%20is%20%F0%9F%90%A7%20some%20%F0%9F%A6%96%20m!x%E2%82%ACd%2520c%C2%BAnt%C2%A3nt"#,
+    #"A%20very%20long%20string%20with%20the%20occassional%20%F0%9F%A6%96%20emoji%20thrown%20in%20for%20good%20measure!%0AA%20very%20long%20string%20with%20the%20occassional%20%F0%9F%A6%96%20emoji%20thrown%20in%20for%20good%20measure!%0AA%20very%20long%20string%20with%20the%20occassional%20%F0%9F%A6%96%20emoji%20thrown%20in%20for%20good%20measure!%0AA%20very%20long%20string%20with%20the%20occassional%20%F0%9F%A6%96%20emoji%20thrown%20in%20for%20good%20measure!%0AA%20very%20long%20string%20with%20the%20occassional%20%F0%9F%A6%96%20emoji%20thrown%20in%20for%20good%20measure!%0AA%20very%20long%20string%20with%20the%20occassional%20%F0%9F%A6%96%20emoji%20thrown%20in%20for%20good%20measure!%0AA%20very%20long%20string%20with%20the%20occassional%20%F0%9F%A6%96%20emoji%20thrown%20in%20for%20good%20measure!%0AA%20very%20long%20string%20with%20the%20occassional%20%F0%9F%A6%96%20emoji%20thrown%20in%20for%20good%20measure!"#
+  ]
+  suite.benchmark("String.urlDecoded") {
+    for string in urlDecoded_strings {
+      blackHole(string.percentDecoded)
+    }
+  }
+}
