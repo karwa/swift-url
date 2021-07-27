2aff2525843a5b451ab287f6b7b5d077d518c494
Karl <5254025+karwa@users.noreply.github.com>
2021-07-27 09:00:41 +0200

    [Utils] Add internal utilities for collapsing consecutive elements, trimming segments. Renamed 'Collection+trim.swift' -> 'BidirectionalCollection+trim.swift'. Renamed some collection extensions tests.

---
+++ Sources/WebURL/Util/BidirectionalCollection+suffix.swift
@@ -0,0 +1,31 @@
+// Copyright The swift-url Contributors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+extension BidirectionalCollection {
+
+  /// Returns the slice of this collection's trailing elements which match the given predicate.
+  ///
+  /// If no elements match the predicate, the returned slice is empty, from `endIndex..<endIndex`.
+  ///
+  @usableFromInline
+  internal func suffix(while predicate: (Element) -> Bool) -> SubSequence {
+    var i = endIndex
+    while i > startIndex {
+      let beforeI = index(before: i)
+      guard predicate(self[beforeI]) else { return self[i..<endIndex] }
+      i = beforeI
+    }
+    return self[startIndex..<endIndex]
+  }
+}
